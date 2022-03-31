24b0712e78abf59c8717894e4f22bab821baffe2
juan <j>
2022-03-31 16:51:55 -0500

    empresas

--- bad.js
+++ bad.js
@@ -5,5 +5,20 @@ function escuchar(event){
     console.log("hizo click");
     let ciudad=document.getElementById("ciudad").value;
     console.log(ciudad)
-    
+    {
+        if (ciudad=="Medellin"){
+            let Ciudad="Agosto 8";
+            alert ("El concierto en " +ciudad+ " es el "+Ciudad)
+        }
+        else if (ciudad=="Bogota"){
+            let Ciudad="Agosto 11";
+            alert ("El concierto en " +ciudad+ " es el "+Ciudad)
+        }
+        else if (ciudad=="Cali"){
+            let Ciudad="Agosto 15";
+            alert ("El concierto en " +ciudad+ " es el "+Ciudad)
+        }else{
+            alert ("perdon pero no hay ningun concierto para "+ciudad)
+        }
+    }
 }
