3cebcfe03fc6f90a7da92ade53fcd82d4393d6e7
juan jose gomez bernal <jgomezbernal72@gmail.com>
2022-04-06 17:05:00 -0500

    empresas

---
+++ epm.js
@@ -0,0 +1,33 @@
+let etiquetaBoton=document.getElementById("boton");
+etiquetaBoton.addEventListener("click",escuchar);
+function escuchar(event){
+    event.preventDefault();
+    console.log("hizo click");
+    let empanada=document.getElementById("empanada").value;
+    console.log(empanada);
+    let nombre=document.getElementById("nombre").value;
+    console.log(nombre);
+    let cedula=document.getElementById("cedula").value;
+    console.log(cedula)
+    let correo=document.getElementById("correo").value;
+    console.log(correo)
+    let cantidad=document.getElementById("cantidad").value;
+    console.log(cantidad)
+
+    if (empanada=="empanada de carne"){
+        let empanada= 2100;
+        let valor=empanada*cantidad;
+        alert("El total por empanadas es $"+valor)
+    }
+    if (empanada=="empanada de pollo"){
+        let empanada= 2500;
+        let valor=empanada*cantidad;
+        alert("El total por empanadas es $"+valor)
+    }
+    if (empanada=="empanada de papa"){
+        let empanada= 2000;
+        let valor=empanada*cantidad;
+        alert("El total por empanadas es $"+valor)
+    }
+    
+}
